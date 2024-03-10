0bbe512e73dbf7b752888810b2d1a5d8315cf50d
kdachev97 <kdachev97@gmail.com>
2024-03-10 14:49:37 +0200

    add edit and delete functionality

--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -1,16 +1,17 @@
 const facilityController = require('express').Router();
 
+const { hasRole } = require('../middlewares/guards');
 const { createFacility, getAllFacilities, addFacilities } = require('../services/facilityService');
 const { getById } = require('../services/roomService');
 
 
-facilityController.get('/create', (req, res) => {
+facilityController.get('/create', hasRole('admin'), (req, res) => {
   res.render('createFacility', {
     title: 'Create New Facility'
   });
 });
 
-facilityController.post('/create', async (req, res) => {
+facilityController.post('/create', hasRole('admin'), async (req, res) => {
   try {
     await createFacility(req.body.label, req.body.iconUrl);
     res.redirect('/catalog');
@@ -25,6 +26,11 @@ facilityController.post('/create', async (req, res) => {
 facilityController.get('/:roomId/decorateRoom', async (req, res) => {
   const roomId = req.params.roomId;
   const room = await getById(roomId);
+
+  if (!req.user || req.user._id !== room.owner) {
+    return res.redirect('/auth/login');
+  }
+
   const facilities = await getAllFacilities();
   facilities.forEach(f => {
     if ((room.facilities || []).some(id => id.toString() == f._id.toString())) {
@@ -40,6 +46,13 @@ facilityController.get('/:roomId/decorateRoom', async (req, res) => {
 });
 
 facilityController.post('/:roomId/decorateRoom', async (req, res) => {
+  const roomId = req.params.roomId;
+  const room = await getById(roomId);
+
+  if (!req.user || req.user._id !== room.owner) {
+    return res.redirect('/auth/login');
+  }
+
   await addFacilities(req.params.roomId, Object.keys(req.body));
 
   res.redirect('/facility/' + req.params.roomId + '/decorateRoom');
